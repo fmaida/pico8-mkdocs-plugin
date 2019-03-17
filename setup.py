8fac0e651ef41a10d338566f659ad26ba09e425b
Francesco Maida <francesco.maida@gmail.com>
2019-03-17 12:49:24 +0100

    First working version

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 from setuptools import setup
 
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
-    name='Pico-8',
+    name='pico8-mkdocs-plugin',
     version='1.0.0',
     packages=['pico8'],
     url='https://github.com/fmaida/pico8-mkdocs-plugin',
@@ -9,6 +14,8 @@ setup(
     author='Francesco Maida',
     author_email='francesco.maida@gmail.com',
     description='This plugin allows to embed a Pico-8 web player in a mkdocs generated page.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     install_requires=['mkdocs'],
 
     # The following rows are important to register your plugin.
